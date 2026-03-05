569511cb3262c1f0f9eec9f700786076cf3bfd01
Anjali Sharma <anjalivce19@gmail.com>
2026-03-05 12:29:59 +0530

    sql queries

---
+++ PostgreSQL-advacned/queries.sql
@@ -0,0 +1,117 @@
+-- JOINS
+
+-- Joins are used to combine rows from two or more tables based on a related column
+-- used when we want some data which have some relation
+
+
+-- How to create table
+-- created a users table
+
+--------------------------------------- tables are created----------------------.
+
+-- CREATE TABLE customers(
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     email TEXT UNIQUE
+-- )
+
+-- CREATE TABLE productOrder(
+--     id SERIAL PRIMARY KEY,
+--     user_id INT REFERENCES customers(id),
+--     product TEXT,
+--     amount NUMERIC
+-- )
+
+-- -- ----------------------------Insert data into tables --------------------------
+
+-- INSERT INTO customers (name, email) 
+-- VALUES 
+--     ('Anjali','anjali@example.com'),
+--    ( 'Nitish','ni@example.com'),
+--    ('Divanshi','div@example.com')
+-- INSERT INTO productOrder (user_id, product, amount)
+-- VALUES 
+--     (1, 'Laptop', 2000000),
+--     (2, 'Mouse', 1000),
+--    ( 3, 'Keyboard', 2000)
+
+-----------------------------------------Show the data----------------------------
+-- SELECT * FROM customers
+-- SELECT * FROM productorder
+
+ --UPDATE customers set id=4 where id=3
+
+-- -------------------------------------- JOINS--------------------
+
+-- -------------------------INNER JOIN
+
+
+-- SELECT * 
+-- from customers as u
+-- INNER JOIN productOrder as o
+-- ON u.id= o.id
+
+-- inner join gives us the mached data in both the tables -> return only match rows.
+
+
+-- LEFT JOIN
+
+-- returns all records from the left table and the matched records from the right table.
+
+-- SELECT c.name, p.product
+-- FROM customers AS c
+-- LEFT JOIN productorder AS p
+-- ON c.id= p.id
+
+-- same as left -> returns all records from right and only matched data from left table
+
+-- SELECT c.name, p.product
+-- FROM customers AS c
+-- RIGHT JOIN productorder AS p
+-- ON c.id= p.user_id
+
+-- Full joins
+ -- returns all recods when there is a match in either left or right table
+-- SELECT * 
+-- FROM customers as c
+-- FULL OUTER JOIN productorder as p
+-- ON c.id= p.id
+
+-- Create index----
+
+-- CREATE UNIQUE INDEX idx_user_email ON customers(email)
+
+--- This will be fast because of index----
+-- If we have large data then it will be helpfull"--
+-- index should be unique----
+
+-- SELECT name from customers where email ='anjali@example.com'
+
+
+
+-----------------------------TRANSACTIONS-------------------------------
+
+-- CREATE TABLE accounts(
+--     id SERIAL PRIMARY KEY,
+--     name TEXT ,
+--     balance NUMERIC CHECK(balance >=0)
+-- )
+
+-- INSERT INTO accounts(name, balance) VALUES
+
+-- ('Rahul',6000000),
+-- ('Nitish', 2000000000)
+-- SELECT * from accounts
+
+-- UPDATE accounts SET balance = balance- 2008990 WHERE name= 'Rahul'
+
+
+--  BEGIN;
+--  UPDATE accounts SET balance= balance+345566 WHERE name='Nitish';
+--  UPDATE accounts SET balance= balance - 567432 WHERE name='Anjali';
+--  COMMIT;
+
+
+----------------------JSONB--------------------
+
+-- in postgres data store in the binary json
---
+++ PostgreSQL-advacned/readme.txt
@@ -0,0 +1,33 @@
+1. JOINS in PostgresSQL:
+-> Joins are used to compare the data from two tables.
+-> a. INNER JOIN
+     -> When we want data which is common in both the tables.
+     -> it returns the records which are common in both the tables.
+-> b. LEFT JOIN
+     -> When we want the all records from left table and common matched rows from other table.
+     -> we need to follow the pattern in this -> the first table we mentioned that will be treated as Left table  
+
+-> c. RIGHT JOIN
+     ->When we want the all records from right table and common matched rows from other table.
+     
+-> d. FULL OUTER JOIN
+     -> it returns the all records from both the tables, if any table has null data/ rows it will mentioned as null.
+
+
+2. INDEXES: 
+-> Indexes improve speed
+----Without Index-> full table scan
+----With Index -> fast lookup
+
+3. TRANSACTIONS:
+-> transactions are used to execute a group of SQL operations as a single unit of work.
+-> if one operations fails the entire transaction fails.
+-> This ensures the ACID properties.
+-> ACID:
+-> Atomicity -> ALL Or Nothing -> all should succesfully complete-> COMMIT
+                               -> if one fails then entire transaction -> ROLLBACK.
+-> Consistency -> Database transfer from one valid state to other valid state
+               -> Foreign key must remain valid
+               -> if rule break -> transaction fails.
+-> Isolation -> transaction will not interfare each other incorrectly.
+-> Durability -> after Commit data is permanently saved(even if system fails)
